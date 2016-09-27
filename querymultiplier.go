8ada200c463a8f952d92800dbb69990e3fc2de81
Jonathan Levin <mail@jonathanlevin.co.uk>
2016-09-27 15:06:21 +0100

    Update querymultiplier.go

--- querymultiplier.go
+++ querymultiplier.go
@@ -1,31 +1,35 @@
-ackage main
+package main
 
 import (
     "fmt"
     "os"
-    "strconv"
     "math"
+    "flag"
 )
 
 func main() {
-//var limit_rows, limit_total int
-querystring := os.Args[1]
-limit_rows, _ := strconv.Atoi(os.Args[2])
-limit_total, _ := strconv.Atoi(os.Args[3])
-looper := int(math.Ceil(float64(limit_total)/float64(limit_rows)))
+flags := flag.NewFlagSet("",flag.ExitOnError)
+querystring := flags.String("",os.Args[1],"The desired query to multiply")
+rows := flags.Int("rows", 100, "The rows or chunk size.") 
+minimum := flags.Int("min", 1, "The position you want the rows to start from.")
+maximum := flags.Int("max", 100, "The maxiumum rows to change or the final id on the column you want.")
+keytype := flags.String("keytype", "limit", "Enter 'limit' or a specific column name like 'id'")
+flags.Parse(os.Args[2:])
+
+looper := int(math.Ceil(float64(*maximum-*minimum)/float64(*rows)))
 
     for i := 0; i < looper; i++ {
-        if ( os.Args[4] == "LIMIT" ) || ( os.Args[4] == "limit") {
+        if ( *keytype == "LIMIT" ) || ( *keytype == "limit") {
                 if ( i == looper-1) {
-                        fmt.Printf("%s LIMIT %d,%d;\n", querystring, limit_rows*i, limit_total-(limit_rows*i))
+                        fmt.Printf("%s LIMIT %d,%d;\n", *querystring, (*rows*i)+*minimum, *maximum-((*rows*i)+*minimum))
                 } else {
-                        fmt.Printf("%s LIMIT %d,%d;\n", querystring, limit_rows*i, limit_rows) 
+                        fmt.Printf("%s LIMIT %d,%d;\n", *querystring, (*rows*i)+*minimum, *rows) 
                 }
         } else {
                 if ( i == looper-1) {
-                        fmt.Printf("%s WHERE %s between %d and %d;\n", querystring, os.Args[4], (limit_rows*i)+1, limit_total)
+                        fmt.Printf("%s WHERE %s between %d and %d;\n", *querystring, *keytype, (*rows*i)+*minimum, *maximum)
                 } else {
-                        fmt.Printf("%s WHERE %s between %d and %d;\n", querystring, os.Args[4], (limit_rows*i)+1, limit_rows*(i+1))
+                        fmt.Printf("%s WHERE %s between %d and %d;\n", *querystring, *keytype, (*rows*i)+*minimum, *rows*(i+1)+*minimum)
                 }
         }
         }
