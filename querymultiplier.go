d3fd9b1a1d45551318df1d87818a8cfe5c31d963
Jonathan Levin <mail@jonathanlevin.co.uk>
2016-09-27 15:14:23 +0100

    Update querymultiplier.go

--- querymultiplier.go
+++ querymultiplier.go
@@ -16,20 +16,20 @@ maximum := flags.Int("max", 100, "The maxiumum rows to change or the final id on
 keytype := flags.String("keytype", "limit", "Enter 'limit' or a specific column name like 'id'")
 flags.Parse(os.Args[2:])
 
-looper := int(math.Ceil(float64(*maximum-*minimum)/float64(*rows)))
+looper := int(math.Ceil(float64(*maximum-*minimum+1)/float64(*rows)))
 
     for i := 0; i < looper; i++ {
         if ( *keytype == "LIMIT" ) || ( *keytype == "limit") {
                 if ( i == looper-1) {
-                        fmt.Printf("%s LIMIT %d,%d;\n", *querystring, (*rows*i)+*minimum, *maximum-((*rows*i)+*minimum))
+                        fmt.Printf("%s LIMIT %d,%d;\n", *querystring, (*rows*i)+*minimum-1, *maximum-((*rows*i)+*minimum)+1)
                 } else {
-                        fmt.Printf("%s LIMIT %d,%d;\n", *querystring, (*rows*i)+*minimum, *rows) 
+                        fmt.Printf("%s LIMIT %d,%d;\n", *querystring, (*rows*i)+*minimum-1, *rows) 
                 }
         } else {
                 if ( i == looper-1) {
                         fmt.Printf("%s WHERE %s between %d and %d;\n", *querystring, *keytype, (*rows*i)+*minimum, *maximum)
                 } else {
-                        fmt.Printf("%s WHERE %s between %d and %d;\n", *querystring, *keytype, (*rows*i)+*minimum, *rows*(i+1)+*minimum)
+                        fmt.Printf("%s WHERE %s between %d and %d;\n", *querystring, *keytype, (*rows*i)+*minimum, *rows*(i+1)+*minimum-1)
                 }
         }
         }
