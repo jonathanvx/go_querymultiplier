04694f7901f99c1a761f94a42714ee1d843d8683
Jonathan Levin <mail@jonathanlevin.co.uk>
2016-09-27 12:10:04 +0100

    Update querymultiplier.go

--- querymultiplier.go
+++ querymultiplier.go
@@ -1,4 +1,4 @@
-package main
+ackage main
 
 import (
     "fmt"
@@ -12,13 +12,21 @@ func main() {
 querystring := os.Args[1]
 limit_rows, _ := strconv.Atoi(os.Args[2])
 limit_total, _ := strconv.Atoi(os.Args[3])
-looper := math.Ceil(float64(limit_total)/float64(limit_rows))
+looper := int(math.Ceil(float64(limit_total)/float64(limit_rows)))
 
-    for i := 0; i < int(looper); i++ {
+    for i := 0; i < looper; i++ {
         if ( os.Args[4] == "LIMIT" ) || ( os.Args[4] == "limit") {
-                fmt.Printf("%s LIMIT %d,%d;\n", querystring, limit_rows*i, limit_rows)
+                if ( i == looper-1) {
+                        fmt.Printf("%s LIMIT %d,%d;\n", querystring, limit_rows*i, limit_total-(limit_rows*i))
+                } else {
+                        fmt.Printf("%s LIMIT %d,%d;\n", querystring, limit_rows*i, limit_rows) 
+                }
         } else {
-                fmt.Printf("%s WHERE %s between %d and %d;\n", querystring, os.Args[4], (limit_rows*i)+1, limit_rows*(i+1))
+                if ( i == looper-1) {
+                        fmt.Printf("%s WHERE %s between %d and %d;\n", querystring, os.Args[4], (limit_rows*i)+1, limit_total)
+                } else {
+                        fmt.Printf("%s WHERE %s between %d and %d;\n", querystring, os.Args[4], (limit_rows*i)+1, limit_rows*(i+1))
+                }
         }
         }
 }
